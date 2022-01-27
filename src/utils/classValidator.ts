5915957878d6042195956e893808087bcd6af498
Michel Calheiros <michel@calheiros.dev>
2022-01-27 03:35:37 -0300

    feat: added front end friendly validation error messages and removed a sanitization bug

--- src/utils/classValidator.ts
+++ src/utils/classValidator.ts
@@ -2,30 +2,76 @@ import { Request, Response, NextFunction, RequestHandler } from 'express';
 import { validate, ValidationError } from 'class-validator';
 import { plainToClass } from 'class-transformer';
 import { sanitize } from 'class-sanitizer';
+
 import HttpStatusCode from '../types/http-status';
 
 type Constructor<T> = { new (): T };
-type GenericObject = { [key: string]: any };
 
 function classValidatorMiddleware<T>(type: Constructor<T>): RequestHandler {
     return (req, res, next) => {
         const inputObject: any = plainToClass(type, req.body);
         validate(inputObject, { skipMissingProperties: false }).then((errors: ValidationError[]) => {
             if (errors.length > 0) {
-                res.status(HttpStatusCode.BAD_REQUEST).json({ validationErrors: errors });
+                const { validationErrorsHTML, validationErrorsHuman, validationErrors } =
+                    getValidationErrorMessages(errors);
+
+                res.status(HttpStatusCode.BAD_REQUEST).json({
+                    validationErrorsHTML,
+                    validationErrorsHuman,
+                    validationErrors,
+                });
+
                 next();
             } else {
+                // if there are no validation errors, sanitize input and continue to the controller
                 sanitize(inputObject);
-                req.body = JSON.stringify(inputObject);
+                req.body = inputObject;
                 next();
             }
         });
     };
 }
 
+type UnprocessedValidationErrors = {
+    [key: string]: string[];
+};
+
+const getValidationErrorMessages = (errors: ValidationError[]) => {
+    const unprocessedErrors: UnprocessedValidationErrors = errors.reduce((acc, cur) => {
+        return {
+            ...acc,
+            [cur.property]: Object.keys(cur.constraints).map((ck) => cur.constraints[ck]),
+        };
+    }, {});
+    const validationErrorsHTML = Object.keys(unprocessedErrors)
+        .map(
+            (key: string) =>
+                `&bull; ${key}:\r\n${unprocessedErrors[key]
+                    .map((text: string) => `&emsp;&#10060; ${text}`)
+                    .join('\r\n')}`,
+        )
+        .join('\r\n');
+
+    const validationErrorsHuman = Object.keys(unprocessedErrors)
+        .map(
+            (key: string) =>
+                `${key.toUpperCase()}: ${unprocessedErrors[key].map((text: string) => `${text}`).join(', ')}`,
+        )
+        .join('\r\n');
+
+    const validationErrors = errors;
+
+    return {
+        validationErrorsHTML,
+        validationErrorsHuman,
+        validationErrors,
+    };
+};
+
 function validationError(err: Error, req: Request, res: Response, next: NextFunction) {
     if (err instanceof Array && err[0] instanceof ValidationError) {
-        res.status(400).json({ errors: err }).end();
+        res.status(400).json({ errors: err });
+        next();
     } else {
         next(err);
     }
