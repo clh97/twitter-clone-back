2738fc787be83724d8ed554017c63ece48d84f07
Michel Calheiros <michel@calheiros.dev>
2022-02-05 04:12:10 -0300

    feat: added get own user profile endpoint

--- src/controllers/user-profile.ts
+++ src/controllers/user-profile.ts
@@ -6,6 +6,8 @@ import { QueryFailedError } from 'typeorm';
 import { PostgresError, handlePostgresError } from '../errors/typeorm';
 import { UserProfile } from '../types/user-profile';
 import UserProfileService from '../services/user-profile';
+import { authenticatedRequest } from '../utils/jwt';
+import { JwtPayload } from 'jsonwebtoken';
 
 class ProfileController extends RouteConfig {
   prefix = 'profile';
@@ -22,6 +24,27 @@ class ProfileController extends RouteConfig {
     this.app.get(`/${this.prefix}/:id`, async (req: express.Request, res: express.Response) => {
       try {
         const userId = parseInt(req.params.id as string);
+        const profile: UserProfile = await this.getProfileByUserId(userId);
+        res.status(HttpStatusCode.OK).send({ profile });
+      } catch (err) {
+        const errorMessage = { error: err.message };
+
+        if (err instanceof QueryFailedError) {
+          const error: PostgresError = handlePostgresError(err);
+          res.status(error.statusCode).send({ error: error.message });
+          throw error;
+        }
+
+        res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(errorMessage);
+        throw err;
+      }
+    });
+
+    // get user own profile
+    this.app.get(`/${this.prefix}`, [authenticatedRequest], async (req: express.Request, res: express.Response) => {
+      try {
+        const jwtPayload: JwtPayload = req.decodedToken as JwtPayload;
+        const userId: number = jwtPayload.id;
         const profile: UserProfile = await this.getProfileByUserId(userId);
         res.status(HttpStatusCode.OK).send({ profile });
       } catch (err) {
